5eea63bdecb284260f85a7bad658c7831f22e4df
Thomas L <thomas08150@hotmail.fr>
2021-01-21 17:24:17 +0100

    test21.01.21

---
+++ EchoCarbone/src/Form/AdminType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Admin;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdminType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname')
+            ->add('lastname')
+            ->add('email')
+            ->add('password')
+            ->add('role')
+            ->add('image')
+            ->add('pseudo')
+            ->add('presentation')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Admin::class,
+        ]);
+    }
+}
